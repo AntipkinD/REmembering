3a7e875b12a8f414bbb155102ad955ea9a637376
mushroomvoodoo <normeeboy@gmail.com>
2023-09-07 11:31:05 +0300

    Добавьте файлы проекта.

---
+++ REmembering/Program.cs
@@ -0,0 +1,37 @@
+﻿double Artem(double a, double b)
+{
+    double k = 0;
+    switch (b)
+    {
+        case < 0:
+            if (a > 0)
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    k += b;
+                }
+                break;
+            }
+            else
+            {
+                for (int i = 0; i < -b; i++)
+                {
+                    k += -a;
+                }
+                break;
+            }
+        case > 0:
+            {
+                for (int i = 0; i < b; i++)
+                {
+                    k += a;
+                }
+                break;
+            }
+    }
+    return k;
+}
+Console.WriteLine(Artem(-5, -6));
+Console.WriteLine(Artem(7, -8));
+Console.WriteLine(Artem(-9, 9));
+Console.WriteLine(Artem(556, 326));
